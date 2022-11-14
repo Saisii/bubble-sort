36d0464e0493557810f62a20d61bb4249e53d3bd
Saisii <sethconger@gmail.com>
2022-11-14 12:17:29 +0300

    clean up code

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,46 +1,24 @@
 def bubble_sort(array)
-    n = array.length
-
     sorted = 1
-
-    p array
 
     while sorted > 0 do
 
-        p "#{array}"
         sorted = 0
         array.each_with_index do |num, idx|    
 
-            p "#number: #{num}, idx: #{idx}"
-
-            p "is #{num} greater than #{array[idx +1]}"
-
             if idx < array.length - 1
 
-                p "idx is: #{idx}. length: #{array.length}"
-
-              if num > array[idx + 1]
+                if num > array[idx + 1]
 
                     array[idx], array[idx + 1] = array[idx + 1], array[idx]
-                    sorted += 1
-                    p "#{sorted} times sorted"
-                    p array
-                    puts 
-              end
-
-            end
-            
+                    sorted += 1 
+                end
+            end 
         end
-
-
-
-        
-    end 
-
-
-    p "Final sorted array is #{array}" 
+    end  
     
-
+    array
+    
 end
 
-bubble_sort([1, 2, 3, 4, 5, 6, 7])
+p bubble_sort([1, 3, 100, 7, 2, 3, 1, 0, 8])
