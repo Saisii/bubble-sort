6d5a72714b5d84af57b91274db679f9083b86182
Saisii <sethconger@gmail.com>
2022-11-14 09:59:39 +0300

    working on array.each_with_index

---
+++ bubble_sort.rb
@@ -0,0 +1,32 @@
+def bubble_sort(array)
+    n = array.length
+
+    sorted = 1
+
+    p array
+
+    while sorted > 0 do
+
+        p "#{array}"
+        sorted = 0
+        array.each|num| do    
+
+        #     p "#number: #{num}, index: #{idx}"
+
+        #     #   if number > array[index + 1]
+        #     #       array[index], array[index + 1] = array[index + 1], array[index]
+        #     #       sorted += 1
+        #     #   p "#{sorted} times sorted"
+        #     #   end
+            
+        end
+
+
+
+        
+    end 
+    
+
+end
+
+bubble_sort([4,3,78,2,0,2])
